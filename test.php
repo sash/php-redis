da75b065d118477606ed0e8d400feac031121b57
Alexander (SASh) Alexiev <alexiev@gmail.com>
2011-04-14 13:15:50 +0300

    added binary safety test

--- test.php
+++ test.php
@@ -7,3 +7,7 @@ $redis->empty='';
 var_dump($redis->empty);
 var_dump($redis->unset);
 var_dump($redis->nonempty);
+
+// Test binary safety using UTF8 keys and data (in bulgarian)
+$redis->{'КИРИЛИЦА'} = "ДА";
+var_dump($redis->{'КИРИЛИЦА'});
