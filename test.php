56cf344e4e9169b5ebb84cf4f7fc9257f6fafdcd
Alexander (SASh) Alexiev <alexiev@gmail.com>
2011-04-14 13:29:58 +0300

    tests for not implemented redis functions

--- test.php
+++ test.php
@@ -11,3 +11,10 @@ var_dump($redis->nonempty);
 // Test binary safety using UTF8 keys and data (in bulgarian)
 $redis->{'КИРИЛИЦА'} = "ДА";
 var_dump($redis->{'КИРИЛИЦА'});
+
+// Test __call
+var_dump($redis->zadd("zkey", 1, "one"));
+var_dump($redis->zadd("zkey", 2, "two"));
+var_dump($redis->zadd("zkey", 3, "three"));
+var_dump($redis->zcard("zkey"));
+var_dump($redis->zrevrank("zkey", "two"));
